ec84d0f7dc90877fd46dc73eb21cbd5e8e2735d6
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-17 14:54:57 +0200

    Tableau doule dimension v2

--- displayImage.ts
+++ displayImage.ts
@@ -1,6 +1,6 @@
 function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
-    liste_colors: number[][], width: number, height: number, couleur_transparence: number, 
-    nombre_pixels: number) {
+    liste_colors: number[][], width: number, height: number, couleur_transparence: number,
+    nombre_occurence: number) {
 
     //Définition de la taille de chaque pixel (via fonction DOT_PIXEL)
     let affiche_pixel;
@@ -27,10 +27,10 @@ function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
     }
 
     //Affichage des points 1 par 1
-    for (let i = 0; i < nombre_pixels; i++) {
-        if (liste_colors[(i%100)][i] != couleur_transparence) {
+    for (let i = 0; i < nombre_occurence; i++) {
+        if (liste_colors[(i % 100)][i] != couleur_transparence) {
             LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
-                liste_colors[(i%10)][i], affiche_pixel);
+                liste_colors[i % 6][i % 100], affiche_pixel);
         }
     }
 
