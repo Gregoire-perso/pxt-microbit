eae54d4f425c0124159ace9188f78764703217fc
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-17 17:07:38 +0200

    On peut afficher des images !!!

--- displayImage.ts
+++ displayImage.ts
@@ -1,7 +1,6 @@
 function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
     liste_colors: number[][], width: number, height: number, couleur_transparence: number,
     nombre_occurence: number) {
-
     //Définition de la taille de chaque pixel (via fonction DOT_PIXEL)
     let affiche_pixel;
     switch (taille_pixel) {
@@ -28,13 +27,14 @@ function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
 
     //Affichage des points 1 par 1
     for (let i = 0; i < nombre_occurence; i++) {
-        if (liste_colors[(i % 100)][i] != couleur_transparence) {
+        if (liste_colors[Math.trunc(i / 100)][i % 100] != couleur_transparence) {
             LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
-                liste_colors[i % 6][i % 100], affiche_pixel);
+                liste_colors[Math.trunc(i / 100)][i % 100], affiche_pixel);
         }
     }
 
     //Rafraichissement de la partie de l'écran correspondant à la taille de l'image
-    LCD1IN8.LCD_DisplayWindows(x_depart - taille_pixel, y_depart - taille_pixel,
-        width * taille_pixel, height * taille_pixel);
+    /*LCD1IN8.LCD_DisplayWindows(x_depart - taille_pixel, y_depart - taille_pixel,
+        width * taille_pixel, height * taille_pixel);*/
+    LCD1IN8.LCD_Display()
 }
