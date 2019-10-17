87e9e8a1b9be8afee413b0038d1b8062691d1a99
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-17 21:16:25 +0200

    Test affichage 2 images

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 LCD1IN8.LCD_Init();
 LCD1IN8.LCD_Clear();
-LCD1IN8.LCD_Filling(0)
-/*
+
 let pika_run = displayPikachuRun();
 displayImage(10, 10, 2, pika_run.listOfColor, pika_run.width, pika_run.height,
     pika_run.transparencyColor, pika_run.numberOfOccurence, -1);
-*/
+
 let pika_stay = displayPikachuStay()
 displayImage(10, 10, 2, pika_stay.listOfColor, pika_stay.width, pika_stay.height,
     pika_stay.transparencyColor, pika_stay.numberOfOccurence, -1)
