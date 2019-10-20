ad3e1834fcf92982f5bbc0a5d946b81a4bd5638e
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-20 13:18:57 +0200

    Possibilité d'afficher plusieurs images à la suite

--- main.ts
+++ main.ts
@@ -1,19 +1,18 @@
 LCD1IN8.LCD_Init();
 LCD1IN8.LCD_Clear();
+LCD1IN8.LCD_Filling(0)
 
-let aff = displayAffiche()
-displayImage(5, 5, 1, aff.listOfColor, aff.width, aff.height,
-    aff.transparencyColor, aff.numberOfOccurence, -1);
 
-/*
 let pika_run = displayPikachuRun();
 displayImage(10, 10, 2, pika_run.listOfColor, pika_run.width, pika_run.height,
     pika_run.transparencyColor, pika_run.numberOfOccurence, -1);
 
-control.waitMicros(2000);
+LCD1IN8.LCD_DisplayWindows(8, 8, (pika_run.width - 1) * 2 + 10, (pika_run.height - 1) * 2 + 10);
 
 let pika_stay = displayPikachuStay();
 displayImage(70, 10, 2, pika_stay.listOfColor, pika_stay.width, pika_stay.height,
     pika_stay.transparencyColor, pika_stay.numberOfOccurence, -1);
 
-*/
+LCD1IN8.LCD_DisplayWindows(68, 8, (pika_stay.width - 1) * 2 + 10 + 60, (pika_stay.height - 1) * 2 + 10);
+
+
