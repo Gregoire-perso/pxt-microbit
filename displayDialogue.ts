62cfe73e8aff16b8daaad3039e815e920da70353
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-21 15:20:15 +0200

    Optimisations et bugs divers

--- displayDialogue.ts
+++ displayDialogue.ts
@@ -2,6 +2,6 @@ function displayDialogue(x: number, y: number, text: string, color: number) {
     for (let i = 0; i < text.length; i++) {
         LCD1IN8.DisString(x + i * 7, y, text[i], color)
         LCD1IN8.LCD_DisplayWindows(x + i * 7, y, 7 * text.length + x, y + 15)
-        control.waitMicros(1000)
+
     }
 } 
