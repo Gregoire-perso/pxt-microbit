34b00dc945a1e9807b6196e7ff252ff3d57ebae7
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-21 16:01:37 +0200

    Ajout exemple usage displayDialogue

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ LCD1IN8.LCD_Init();
 LCD1IN8.LCD_Clear();
 
 /*
+Exemple d'affichage d'image
+
 let pika_run = displayPikachuRun();
 displayImage(10, 10, 2, pika_run.listOfColor, pika_run.width, pika_run.height,
     pika_run.transparencyColor, pika_run.numberOfOccurence, -1);
@@ -13,4 +15,12 @@ displayImage(70, 10, 2, pika_stay.listOfColor, pika_stay.width, pika_stay.height
     pika_stay.transparencyColor, pika_stay.numberOfOccurence, -1);
 
 LCD1IN8.LCD_DisplayWindows(68, 8, (pika_stay.width - 1) * 2 + 10 + 60, (pika_stay.height - 1) * 2 + 10);
+*/
+
+/*
+Exemple d'affichage de dialogue
+
+texte = "Coucou tout le monde ! Comment allez-vous ?";
+displayDialogue(5, 5, texte, 0, 10);
+
 */
