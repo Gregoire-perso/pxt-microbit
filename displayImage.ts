f90e913975ceb320dc3a4c79f51c6d02c85a2fbd
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2020-01-07 22:06:26 +0100

    Update displayImage.ts

--- displayImage.ts
+++ displayImage.ts
@@ -1,26 +1,22 @@
-function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
-    liste_colors: number[][], width: number, height: number,
-    couleur_transparence: number, nombre_occurence: number,
-    replace_color: number) {
+function displayImage(x_depart: number, y_depart: number, 
+    picture: { listOfColorNumber: number[][], listOfColor: number[][], paletteOfColors: number[], width: number, height: number, numberOfOccurence: number }, 
+    taille_pixel: number = 1, transparency: boolean = true) {
     /*
     Explication des arguments : 
         x_depart, y_depart --> coordonnée de l'image (voir avec taille pixel)
         taille_pixel --> taille de chaque pixel
-        liste_colors --> liste des couleurs de chaque pixel de l'image
+        list_colors --> liste des couleurs de chaque pixel de l'image
+        palette_colors --> palette de correspondance entre les indices (présents dans 'list_colors') et les couleurs en décimal
         width --> largeur de l'image
         height --> hauteur de l'image
-        couleur_transparence --> couleur de 'liste_colors' à ne pas afficher
         nombre_occurence --> nombre de pixel de l'image
         replace_color --> couleur à afficher à la place de la couleur de transparence
+        transparency --> true: affiche la couleur contenue dans replace_color
     */
 
     //Définition de la taille de chaque pixel (via fonction DOT_PIXEL)
     let affiche_pixel;
     switch (taille_pixel) {
-        case 1:
-            affiche_pixel = DOT_PIXEL.DOT_PIXEL_1;
-            break;
-
         case 2:
             affiche_pixel = DOT_PIXEL.DOT_PIXEL_2;
             break;
@@ -38,19 +34,26 @@ function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
             break;
     }
 
-    //Affichage des points 1 par 1
-    for (let i = 0; i < nombre_occurence; i++) {
-        if (liste_colors[Math.trunc(i / 100)][i % 100] != couleur_transparence) {
-            LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
-                liste_colors[Math.trunc(i / 100)][i % 100], affiche_pixel);
+    let noPx = 0;
+    for(let i = 0; i < (picture.listOfColorNumber.length - 1) * (picture.listOfColorNumber[0].length) + picture.listOfColorNumber[picture.listOfColorNumber.length - 1].length; i++) {
+        if (!transparency || (picture.listOfColor[Math.trunc(i / 100)][i % 100] != 0 && transparency)) {
+            for (let j = 0; j < picture.listOfColorNumber[Math.trunc(i / 100)][i % 100]; j++) {
+                LCD1IN8.DrawPoint(x_depart + (noPx % picture.width) * taille_pixel, y_depart + Math.trunc(noPx / picture.width) * taille_pixel,
+                    picture.paletteOfColors[picture.listOfColor[Math.trunc(i / 100)][i % 100]], affiche_pixel);
+                noPx ++
+            }
         }
 
-        else if (replace_color != -1) {
-            LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
-                replace_color, affiche_pixel);
+        else {
+            noPx += picture.listOfColorNumber[Math.trunc(i / 100)][i % 100]
         }
     }
-
-    //Rafraichissement de la partie de l'écran correspondant à la taille de l'image doit se faire en dehors de cette fonction
-    //test(x_depart - taille_pixel, y_depart - taille_pixel, (width - 1) * taille_pixel + 10, (height - 1) * taille_pixel + 10)
+    
+    LCD1IN8.LCD_DisplayWindows(x_depart - taille_pixel, y_depart - taille_pixel, (picture.width - 1) * taille_pixel + x_depart, (picture.height - 1) * taille_pixel + y_depart)
+    delete picture.listOfColor
+    delete picture.listOfColorNumber
+    delete picture.paletteOfColors
+    delete picture.numberOfOccurence
+    delete picture.width
+    delete picture.height
 }
